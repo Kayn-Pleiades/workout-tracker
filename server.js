1ac07f4011bf224fb301e82ab603d12d3ccdc7bb
unknown <kayn.pleiades@gmail.com>
2021-04-07 16:25:13 -0400

    starting server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static('public'));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set Up Route here
+
+app.listen(PORT, () => console.log(`App now listening at localhost:${PORT}`));
